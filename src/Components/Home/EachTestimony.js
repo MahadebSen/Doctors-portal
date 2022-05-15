eaa7c438d6db65c47965dd9370853d8b7138b144
Mahadeb Sen <mahadebsen237@gmail.com>
2022-05-15 18:46:10 +0530

    testimonial added

---
+++ src/Components/Home/EachTestimony.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const EachTestimony = ({ comment }) => {
+  const { name, address, img, testimony } = comment;
+  return (
+    <div className="p-5 rounded-xl shadow-2xl">
+      <p className="mb-6">{testimony}</p>
+      <div className="flex items-center ml-3">
+        <div class="avatar w-[60px] h-[60px]">
+          <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <img src={img} alt="" />
+          </div>
+        </div>
+        <div className="ml-5">
+          <p className="font-semibold text-lg">{name}</p>
+          <p>{address}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EachTestimony;
--- src/Components/Home/Testimonial/Testimonial.js
+++ src/Components/Home/Testimonial/Testimonial.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import quote from "../../../assets/icons/quote.svg";
+import EachTestimony from "../EachTestimony";
 
 const Testimonial = () => {
-  return <div></div>;
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch("FakeComments.json")
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, []);
+
+  return (
+    <div className="mx-10">
+      <section className="flex justify-between items-center my-24">
+        <div>
+          <p className="text-primary font-bold pb-1">Testimonial</p>
+          <p className="text-3xl text-secondery">What Our Patientsw Says</p>
+        </div>
+        <div>
+          <img className="w-[195px] h-[160px]" src={quote} alt="" />
+        </div>
+      </section>
+      <section className="grid grid-cols-3 gap-10">
+        {comments.map((comment) => (
+          <EachTestimony key={comment._id} comment={comment}></EachTestimony>
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Testimonial;
